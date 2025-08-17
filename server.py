3f7577cb432ec19bf72bde527fd1e21a828cbfd8
Harjas Singh <desibois@outlook.com>
2025-08-17 16:11:47 +0100

    New Branch

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, request, jsonify
+=======
+from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+from adafruit_servokit import ServoKit
+import time
+>>>>>>> c96cd7ce36c9ec54b51eaa85165b02f2dedafc27
 
-app = Flask(__name__)
+relay_pin = 14  
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+GPIO.setmode(GPIO.BCM)    
+GPIO.setup(relay_pin, GPIO.OUT)
 
+<<<<<<< HEAD
 @app.route("/move")
 def move():
     direction = request.args.get("dir")
@@ -16,11 +23,61 @@ def move():
         return jsonify({"movement": 10})
     else:
         return jsonify({"movement": 0})
+=======
+kit = ServoKit(channels=16)
+kit.servo[0].set_pulse_width_range(500, 2500) 
+current_angle = 90
+min_angle, max_angle = 0, 180
+kit.servo[0].angle = current_angle
+>>>>>>> c96cd7ce36c9ec54b51eaa85165b02f2dedafc27
 
-@app.route("/fire")
-def fire_gun():
-    print("FIRE command received")  # logs to console
-    return "FIRING"
+def fire():
+	GPIO.output(relay_pin, GPIO.HIGH)  
+	time.sleep(0.5)                    
+	GPIO.output(relay_pin, GPIO.LOW)   
+		
+try:
+	GPIO.output(relay_pin, GPIO.LOW)
+			
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  # accessible from any device on your network
+	app = Flask(__name__)
+
+	@app.route("/")
+	def home():
+		return render_template("index.html")
+
+	@app.route("/move")
+	def move():
+		global current_angle
+		direction = request.args.get("dir")
+		print(f"Move command received: {direction}")
+		if direction == 'right':
+			current_angle += 10
+			if min_angle <= current_angle <= max_angle:
+				kit.servo[0].angle = current_angle
+				kit.servo[0].angle = current_angle
+			else: current_angle -= 10
+		elif direction == 'left':
+			current_angle -= 10
+			if min_angle <= current_angle <= max_angle:
+				kit.servo[0].angle = current_angle
+				kit.servo[0].angle = current_angle
+			else: current_angle += 10
+		return f"Moved {direction}"
+
+	@app.route("/fire")
+	def fire_gun():
+		print("FIRE command received")
+		GPIO.output(relay_pin, GPIO.HIGH)
+		return "FIRING"
+
+	@app.route("/stopFire")
+	def stop_gun():
+		print("STOP command recieved")
+		GPIO.output(relay_pin, GPIO.LOW)
+		return "STOPPED"
+
+	if __name__ == "__main__":
+		app.run(host="0.0.0.0", port=5000) 
+finally:
+	GPIO.cleanup()
